d9a827b5d5c792aa8258be1344bfd2da9b6b4705
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2024-07-06 16:58:38 +0530

    modified

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,10 @@ import { Outlet } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <main className="w-full h-full flex flex-col first-letter:content-center items-center relative text-white font-nunito">
+      <div className="w-screen h-screen bg-gray-300 fixed -z-10"></div>
       <Outlet />
-    </div>
+    </main>
   );
 };
 
