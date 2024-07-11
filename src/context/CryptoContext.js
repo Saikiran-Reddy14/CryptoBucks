0d1652a1ac935b5603baf55a05071aa1693a9d20
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2024-07-11 17:19:44 +0530

    modified

--- src/context/CryptoContext.js
+++ src/context/CryptoContext.js
@@ -1,7 +1,39 @@
-import { createContext } from "react";
+import { createContext, useLayoutEffect, useState } from "react";
 
 export const CryptoContext = createContext({});
 
 export const CryptoProvider = ({ children }) => {
-  return <CryptoContext.Provider>{children}</CryptoContext.Provider>;
+  const [cryptoData, setCryptoData] = useState();
+
+  const getCryptoData = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-api-key": "CG-Pgviptiz4NS7BA84fMZXP1wB",
+      },
+    };
+
+    try {
+      const response = await fetch(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C%2024h%2C%207d",
+        options
+      );
+      const data = await response.json();
+      console.log(data);
+      setCryptoData(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useLayoutEffect(() => {
+    getCryptoData();
+  }, []);
+
+  return (
+    <CryptoContext.Provider value={{ cryptoData, setCryptoData }}>
+      {children}
+    </CryptoContext.Provider>
+  );
 };
