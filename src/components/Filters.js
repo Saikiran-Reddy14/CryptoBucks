f3c92e0d3bdeb8805b85d4e3f774654d89b770ea
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2024-07-13 18:57:29 +0530

    added currency changing option

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,11 +1,30 @@
 import React from "react";
 import Search from "./Search";
+import submitIcon from "../assets/submit-icon.svg";
 
 const Filters = () => {
   return (
     <div className="w-full h-12 border-2 border-gray-100 rounded-lg flex items-center justify-between relative">
       <Search />
-      <div>currency</div>
+      <div className="flex mr-7">
+        <form className="relative flex items-center font-nunito mr-12">
+          <label
+            htmlFor="currency"
+            className="flex justify-center items-center mr-2 font-bold"
+          >
+            Currency:
+          </label>
+          <input
+            type="text"
+            name="currency"
+            placeholder="usd"
+            className="w-16 rounded bg-gray-200 placeholder:text-gray-100 pl-2 required outline-0 border border-transparent focus:border-cyan leading-4"
+          />
+          <button type="submit" className="ml-1 cursor-pointer">
+            <img src={submitIcon} alt="submitIcon" className="w-full h-auto" />
+          </button>
+        </form>
+      </div>
       <div>sorting</div>
     </div>
   );
